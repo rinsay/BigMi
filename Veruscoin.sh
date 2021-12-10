6088e7a72a6dfdb3510561d43bdc60a8957e1908
rinsay <95890481+rinsay@users.noreply.github.com>
2021-12-10 07:06:59 +0700

    Create Veruscoin.sh

---
+++ Veruscoin.sh
@@ -0,0 +1,5 @@
+#veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RWjVou1SrbSTDJwXBmfMGb3MtXofWwP7pY.Bigmi1 -p x --cpu 4
